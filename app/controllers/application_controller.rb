beb61762a50acb3e8b5704fbe6db70a4aef61cc8
Mateusz Król <kroolikk@gmail.com>
2012-10-29 20:36:45 +0100

    dodany mailer po zapisie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
     if params[:email].present? && params[:email] =~ /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
       @subscribtion = Subscription.new(:email => params[:email])
       if @subscribtion.save
+        Notifier.subscription_notification(@subscribtion).deliver
         redirect_to root_url, notice: "Adres został zapisany. Dziękujemy."
       else
         redirect_to root_url, alert: "Nie udało się zapisac adresu."
